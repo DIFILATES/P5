79d65893b5bc81fdbcd62d97d1c7fa21a8aebbd4
Difilates <pau.diport.i@gmail.com>
2026-05-22 08:16:50 +0200

    ultima classe

--- src/instruments/instrument_dumb.h
+++ src/instruments/instrument_dumb.h
@@ -9,7 +9,7 @@
 namespace upc {
   class InstrumentDumb: public upc::Instrument {
     EnvelopeADSR adsr;
-    unsigned int index;
+    float fase,step;
 	float A;
     std::vector<float> tbl;
   public:
